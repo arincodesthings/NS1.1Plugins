ea3fdb3089dda1468d767c458f57fb7bc0bbe190
xAleXXX007x <AleXXX_007@inbox.ru>
2016-10-19 11:19:07 +0400

    Fixed notes value saving

--- notes+/sh_plugin.lua
+++ notes+/sh_plugin.lua
@@ -8,21 +8,20 @@ WRITINGDATA = WRITINGDATA or {}
 nut.util.include("cl_vgui.lua")
 
 if (CLIENT) then
-	netstream.Hook("receiveNote", function(id, contents, write, placed)
+	netstream.Hook("receiveNote", function(id, contents, write)
 		local note = vgui.Create("noteRead")
 		note:allowEdit(write)
 		note:setText(contents)
 		note.id = id
-		if placed == 1 then
-			note:placed(false, false)
-		else
-			note:placed(true, write)
-		end
 	end)
 else	
 	netstream.Hook("noteSendText", function(client, id, contents)
 		if (string.len(contents) <= NOTELIMIT) then
-			WRITINGDATA[id] = contents
+			for k, v in pairs(nut.item.instances) do
+				if v.uniqueID == "note" and v.id == id then
+					v:setData("text", contents)
+				end
+			end
 		end
 	end)	
 end
