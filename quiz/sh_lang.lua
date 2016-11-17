e612601ea59da1a6a3c0245326282b41728a4bae
xAleXXX007x <AleXXX_007@inbox.ru>
2016-11-17 20:32:51 +0400

    Add files via upload

---
+++ quiz/sh_lang.lua
@@ -0,0 +1,3 @@
+nut.lang.Add("quiz_title", "Вопросы")
+nut.lang.Add("quiz_notification", "При неправильных ответах на вопросы Вы будете отключены от сервера.")
+nut.lang.Add("quiz_submit", "Проверить ответы")
